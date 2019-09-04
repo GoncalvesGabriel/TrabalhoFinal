6d03320fa1bf8e6e38a5e29498623a19f1eafb1d
Gabriel Goncalves <goncalves.gabrielsilva@gmail.com>
2019-09-03 22:59:55 -0300

    Mundando versão min e avanço splash

--- app/src/main/java/br/com/fiap/trabalhofinal/ui/splash/SplashActivity.kt
+++ app/src/main/java/br/com/fiap/trabalhofinal/ui/splash/SplashActivity.kt
@@ -1,13 +1,33 @@
 package br.com.fiap.trabalhofinal.ui.splash
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.view.animation.AnimationUtils
 import br.com.fiap.trabalhofinal.R
+import br.com.fiap.trabalhofinal.ui.main.MainActivity
+import kotlinx.android.synthetic.main.activity_splash.*
 
 class SplashActivity : AppCompatActivity() {
+
+    private val TIME_WAIT_SPLASHSCREEN = 3500L
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+        animar()
+    }
+
+    private fun animar() {
+        val anim = AnimationUtils.loadAnimation(this, R.anim.animacao_splash)
+        anim.reset()
+        ivSplash.clearAnimation()
+        ivSplash.startAnimation(anim)
+        Handler().postDelayed({
+            val nextActivity = Intent(this@SplashActivity, MainActivity::class.java)
+            startActivity(nextActivity)
+            finish()
+        }, TIME_WAIT_SPLASHSCREEN)
     }
 }
